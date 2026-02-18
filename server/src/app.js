ed8be4c0f40ae10333d46e2396942b56654c62aa
Palak Agnihotri <agnihotripalak15@gmail.com>
2026-02-18 16:07:25 +0530

    Implemented all the tasks for Day 1 and Day 2 and added Readme.md which represents all tasks that have been performed

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const helmet = require("helmet")
 
 const healthRoutes = require("./routes/healthroutes")
-
+const authRoutes = require("./routes/authRoutes")
 const app = express()
 
 app.use(cors())
@@ -11,5 +11,5 @@ app.use(helmet())
 app.use(express.json())
 
 app.use("/health", healthRoutes)
-
+app.use("/api/auth", authRoutes)
 module.exports = app
